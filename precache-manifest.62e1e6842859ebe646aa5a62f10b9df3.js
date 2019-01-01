1f42a7eb97e5a38bf627738328945af9f17011f8
johndev86 <johnlee@Johns-MacBook-Pro.local>
2018-12-31 22:06:51 -0500

    Updates

---
+++ precache-manifest.62e1e6842859ebe646aa5a62f10b9df3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "aad46eb2a786671c564e5e248e1f5e62",
+    "url": "/starwars-api/static/media/starback.aad46eb2.jpg"
+  },
+  {
+    "revision": "9c91297e17268fbd21d698ca6e526faa",
+    "url": "/starwars-api/static/media/Starjhol.9c91297e.ttf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/starwars-api/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "aab056996c3665abb2eb",
+    "url": "/starwars-api/static/js/runtime~main.aab05699.js"
+  },
+  {
+    "revision": "25a8ae65ffb36a8e2d2f",
+    "url": "/starwars-api/static/js/main.25a8ae65.chunk.js"
+  },
+  {
+    "revision": "4a66b8655ae633089d38",
+    "url": "/starwars-api/static/js/1.4a66b865.chunk.js"
+  },
+  {
+    "revision": "25a8ae65ffb36a8e2d2f",
+    "url": "/starwars-api/static/css/main.144f27d4.chunk.css"
+  },
+  {
+    "revision": "4a66b8655ae633089d38",
+    "url": "/starwars-api/static/css/1.da85e0ea.chunk.css"
+  },
+  {
+    "revision": "9dd61eb643c19894391d373ac186a52e",
+    "url": "/starwars-api/index.html"
+  }
+];
